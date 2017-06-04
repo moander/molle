917a0fc440a58958858d037b598068dff6f5bdfd
moander <moander@outlook.com>
2017-06-04 12:11:33 +0200

    First commit

---
+++ Assets/DotBehaviour.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+/**
+ * ToDo list
+ * 
+ * [ ] Put 9 dots each on the screen
+ * [x] Move dots around to free spots on the line
+ * [ ] Remove oponent dot on three in a row
+ * [x] Jump when three dots are left
+ */
+
+[Serializable]
+public class DotBehaviour : MonoBehaviour {
+    public string Jau { get; set; }
+
+    public double CurrentScaleSin = 0.0F;
+    public double Scaler = 4.0;
+    public double Speed = 8;
+    public bool MyCanSelect;
+    public Mode CurrentMode = Mode.NotUsed;
+    private Vector3 initialLocalScale;
+    private Vector3 notUsedLocalScale;
+    private Vector3 inUseLocalScale;
+    public float CannotSelectAnimation;
+    private double CannotSelectSin;
+    private static MyContext Context = new MyContext();
+    public bool isWhite = false;
+    private bool NeedUpdate;
+    internal int DotIndex;
+    public List<int> canTouch = new List<int>();
+    internal bool HasThreeInRow;
+
+    internal bool IsCurrentColor
+    {
+        get
+        {
+            return CurrentMode == Mode.IsPlayer && isWhite == Context.IsCurrentPlayerWhite;
+        }
+    }
+
+    internal bool MyIsSelected
+    {
+        get
+        {
+            return Context.CurrentlySelectedDot == this;
+        }
+    }
+
+    /*internal bool HasThreeInARow
+    {
+        get
+        {
+            if(CurrentMode == Mode.IsPlayer)
+            {
+                if((DotIndex & 1))
+                foreach (var i in CurrentlySelectedDot.canTouch)
+                {
+                    Dots[i];
+                }
+            }
+        }
+    }*/
+
+    // Use this for initialization
+    void Start() {
+        MyCanSelect = true;
+        CurrentMode = Mode.NotUsed;
+        char[] aaa = { '(', ')', ' ' };
+        DotIndex = Convert.ToInt32(this.name.Substring(3).Trim(aaa), 10);
+        Context.Dots[DotIndex] = this;
+        initialLocalScale = transform.localScale;
+        notUsedLocalScale = initialLocalScale;
+        inUseLocalScale = new Vector3(initialLocalScale.x * 1.5F, initialLocalScale.y * 4.5F, initialLocalScale.z * 1.5F);
+
+        var CircleIndex = DotIndex % 8;
+        var CircleNumber = DotIndex / 8;
+
+        if ((DotIndex & 1) == 0)
+        {
+            // Corner
+            if (CircleIndex == 0)
+            {
+                canTouch.Add(DotIndex + 7);
+                canTouch.Add(DotIndex + 1);
+            }
+            else if(CircleIndex == 7)
+            {
+                canTouch.Add(DotIndex - 7);
+                canTouch.Add(DotIndex - 1);
+            }
+            else
+            {
+                canTouch.Add(DotIndex + 1);
+                canTouch.Add(DotIndex - 1);
+            }
+        }
+        else
+        {
+            // Middle
+            if(CircleIndex == 7)
+            {
+                canTouch.Add(DotIndex - 7);
+                canTouch.Add(DotIndex - 1);
+            }
+            else
+            {
+                canTouch.Add(DotIndex - 1);
+                canTouch.Add(DotIndex + 1);
+            }
+            
+
+            if (CircleNumber == 0)
+            {
+                canTouch.Add(DotIndex + 8);
+            }
+            else if(CircleNumber == 1)
+            {
+                canTouch.Add(DotIndex + 8);
+                canTouch.Add(DotIndex - 8);
+            }
+            else
+            {
+                canTouch.Add(DotIndex - 8);
+            }
+            
+        }
+    }
+
+    internal bool IsSamePlayer(DotBehaviour other)
+    {
+        return CurrentMode == Mode.IsPlayer && CurrentMode == other.CurrentMode && isWhite == other.isWhite;
+    }
+
+    void OnMouseDown()
+    {
+        if (MyCanSelect)
+        {
+            var DoSwitchPlayer = false;
+
+            if (CurrentMode == Mode.NotUsed)
+            {
+                if (Context.DotsLeftToPlay == 0)
+                {
+                    CurrentMode = Mode.IsPlayer;
+                    isWhite = Context.IsCurrentPlayerWhite;
+
+                    Context.CurrentlySelectedDot.CurrentMode = Mode.NotUsed;
+
+                    DoSwitchPlayer = true;
+                }
+                else
+                {
+                    CurrentMode = Mode.IsPlayer;
+                    isWhite = Context.IsCurrentPlayerWhite;
+
+                    MyCanSelect = false;
+
+                    if( --Context.DotsLeftToPlay == 0 )
+                    {
+                        foreach(var dot in Context.Dots)
+                        {
+                            dot.MyCanSelect = false;
+                        }
+                    }
+
+                    if(isWhite)
+                    {
+                        Context.DotsLeftWhite++;
+                    } else
+                    {
+                        Context.DotsLeftBlack++;
+                    }
+                    
+                    DoSwitchPlayer = true;
+                }
+            }
+            else if (CurrentMode == Mode.IsPlayer)
+            {
+                Context.CurrentlySelectedDot = MyIsSelected ? null : this;
+            }
+
+            Context.UpdateDots(this, DoSwitchPlayer);
+        }
+        else
+        {
+            CannotSelectAnimation = 0.8F;
+            CannotSelectSin = 0.0F;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 baseSize = initialLocalScale;
+
+        if (CurrentMode == Mode.NotUsed)
+        {
+            baseSize = notUsedLocalScale;
+            GetComponent<Renderer>().material.color = new Color(1.0F, 0.0F, 0.0F); //C#
+        }
+        else if (CurrentMode == Mode.IsPlayer)
+        {
+            baseSize = inUseLocalScale;
+            if(this.isWhite)
+            {
+                GetComponent<Renderer>().material.color = new Color(1.0F, 1.0F, 1.0F); //C#
+            } else
+            {
+                GetComponent<Renderer>().material.color = new Color(0.0F, 0.0F, 0.0F); //C#
+            }
+        }
+
+        if(MyIsSelected)
+        {
+            CurrentScaleSin += Speed * 4.0F * Time.deltaTime;
+
+            var ScaleValue = ((Math.Sin(CurrentScaleSin * Scaler * Math.PI) + 1.0) * 0.5) + 0.9;
+            baseSize = new Vector3(baseSize.x * (float)ScaleValue, baseSize.y, baseSize.x * (float)ScaleValue);
+        }
+        else if (MyCanSelect)
+        {
+            CurrentScaleSin += Speed * Time.deltaTime;
+
+            var ScaleValue = ((Math.Sin(CurrentScaleSin * Scaler * Math.PI) + 1.0) * 0.5) + 0.9;
+            baseSize = new Vector3(baseSize.x * (float)ScaleValue, baseSize.y, baseSize.x * (float)ScaleValue);
+        }
+
+        if(CannotSelectAnimation > 0.0F)
+        {
+            CannotSelectAnimation -= Time.deltaTime;
+            CannotSelectSin += Speed * 10.0F * Time.deltaTime;
+
+            var ScaleValue = ((Math.Sin(CannotSelectSin * Scaler * Math.PI) + 1.0) * 0.5) + 0.9;
+            baseSize = new Vector3(baseSize.x * (float)ScaleValue, baseSize.y, baseSize.x * (float)ScaleValue);
+        }
+
+        transform.localScale = baseSize;
+    }
+
+    public enum Mode
+    {
+        NotUsed,
+        IsPlayer,
+    }
+}
---
+++ Assets/MyContext.cs
@@ -0,0 +1,132 @@
+﻿using Boo.Lang;
+using System.Linq;
+using System;
+
+internal class MyContext
+{
+    public DotBehaviour[] Dots = new DotBehaviour[24];
+    public bool IsCurrentPlayerWhite = true;
+    public int DotsLeftToPlay = 8;
+    
+    public int DotsLeftBlack = 0;
+    public int DotsLeftWhite = 0;
+
+    internal DotBehaviour CurrentlySelectedDot;
+
+    public void UpdateDots(DotBehaviour currentDot, bool DoSwitchPlayer)
+    {
+        var oldHasThreeInRow = currentDot.HasThreeInRow;
+
+        foreach (var dot in Dots)
+        {
+            dot.HasThreeInRow = false;
+        }
+
+        for (var i = 1; i < Dots.Length; i += 2)
+        {
+            var dot = Dots[i];
+
+            var CircleIndex = dot.DotIndex % 8;
+
+            if (CircleIndex == 7)
+            {
+                UpdateAndSetThreeInRow(dot, -7, -1);
+            }
+            else
+            {
+                UpdateAndSetThreeInRow(dot, -1, 1);
+            }
+        }
+
+        for (var i = 9; i <= 15; i += 2)
+        {
+            var dot = Dots[i];
+
+            UpdateAndSetThreeInRow(dot, -8, 8);
+        }
+
+        if (DoSwitchPlayer && !oldHasThreeInRow && currentDot.HasThreeInRow)
+        {
+            foreach (var dot in Dots)
+            {
+                dot.MyCanSelect = dot.CurrentMode == DotBehaviour.Mode.IsPlayer &&
+                                  dot.isWhite != currentDot.isWhite &&
+                                  !dot.HasThreeInRow;
+            }
+
+            return;
+        }
+
+        if (DoSwitchPlayer)
+        {
+            IsCurrentPlayerWhite = !IsCurrentPlayerWhite;
+            CurrentlySelectedDot = null;
+            foreach (var dot in Dots)
+            {
+                dot.CurrentScaleSin = 0.0F;
+            }
+        }
+
+
+        var DotsLeftCurrentPlayer = IsCurrentPlayerWhite ? DotsLeftWhite : DotsLeftBlack;
+
+        if (DotsLeftToPlay > 0)
+        {
+            // Put new anywhere free
+        }
+        else if (CurrentlySelectedDot == null)
+        {
+            // Select dot to move
+            foreach (var dot in Dots)
+            {
+                dot.MyCanSelect = dot.IsCurrentColor;
+            }
+
+            // Second select dot to move check
+            foreach (var dot in Dots.Where(p=>p.MyCanSelect))
+            {
+                dot.MyCanSelect = dot.canTouch.Select(p => this.Dots[p])
+                    .Any(p => p.CurrentMode == DotBehaviour.Mode.NotUsed);
+
+            }
+        }
+        else
+        {
+            // Select where to place selected dot
+            if (DotsLeftCurrentPlayer > 3)
+            {
+                // Select dot to move
+                foreach (var dot in Dots)
+                {
+                    dot.MyCanSelect = false;
+                }
+
+                foreach(var i in CurrentlySelectedDot.canTouch)
+                {
+                    var dot = Dots[i];
+                    dot.MyCanSelect = dot.CurrentMode == DotBehaviour.Mode.NotUsed;
+                }
+            }
+            else if (DotsLeftCurrentPlayer == 3)
+            {
+                // Select dot to move
+                foreach (var dot in Dots)
+                {
+                    dot.MyCanSelect = dot.CurrentMode == DotBehaviour.Mode.NotUsed;
+                }
+            }
+        }
+    }
+
+    void UpdateAndSetThreeInRow(DotBehaviour dot, int index1, int index2)
+    {
+        if (dot.IsSamePlayer(Dots[dot.DotIndex - index1]) && dot.IsSamePlayer(Dots[dot.DotIndex - index2]))
+        {
+            Dots[dot.DotIndex - index1].HasThreeInRow = true;
+            Dots[dot.DotIndex - index2].HasThreeInRow = true;
+            dot.HasThreeInRow = true;
+        }
+    }
+
+
+}
